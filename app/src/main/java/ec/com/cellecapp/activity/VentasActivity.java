679276d729b2a674d99dee9fa06676e57ea4bfae
Walther Joao <waltherjoao@gmail.com>
2022-08-08 20:36:02 -0500

    Login con google

---
+++ app/src/main/java/ec/com/cellecapp/activity/VentasActivity.java
@@ -0,0 +1,16 @@
+package ec.com.cellecapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ec.com.cellecapp.R;
+
+public class VentasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventas);
+    }
+}
